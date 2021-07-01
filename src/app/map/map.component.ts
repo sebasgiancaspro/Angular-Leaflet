9db26a91ff48a22e3d5073ab26685a0dff8e8b2c
Sebastian <sebasgiancaspro@gmail.com>
2021-07-01 09:47:48 -0300

    Uso de Leaflet con Angular

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,70 @@
+import { Component, AfterViewInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+
+export class MapComponent implements AfterViewInit {
+  map;
+
+  // retrieve from https://gist.github.com/ThomasG77/61fa02b35abf4b971390
+  smallIcon = new L.Icon({
+    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon.png',
+    iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon-2x.png',
+    iconSize:    [25, 41],
+    iconAnchor:  [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    shadowSize:  [41, 41]
+  });
+
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.createMap();
+  }
+
+  createMap() {
+    const edifCondor = {
+      lat: -34.58501,  
+      lng: -58.37095,
+    };
+
+    const zoomLevel = 16;
+
+    this.map = L.map('map', {
+      center: [edifCondor.lat, edifCondor.lng],
+      zoom: zoomLevel
+    });
+
+    const mainLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      minZoom: 3,
+      maxZoom: 18,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
+
+    mainLayer.addTo(this.map);
+    const descriptionWikipedia = `
+    El Edificio Cóndor es la sede de la Fuerza Aérea Argentina. Se encuentra en el área de Puerto Nuevo, 
+    en la ciudad de Buenos Aires, Argentina.`;
+    const popupOptions = {
+      coords: edifCondor,
+      text: descriptionWikipedia,
+      open: true
+    };
+    this.addMarker(popupOptions);
+  }
+
+  addMarker({coords, text, open}) {
+    const marker = L.marker([coords.lat, coords.lng], { icon: this.smallIcon });
+    if (open) {
+      marker.addTo(this.map).bindPopup(text).openPopup();
+    } else {
+      marker.addTo(this.map).bindPopup(text);
+    }
+  }
+
+}
